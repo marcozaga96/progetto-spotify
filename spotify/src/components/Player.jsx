544e42f42529c9d4947d74785794590e73afa7c4
Marco Zagaria <marcozagaria@live.com>
2024-09-06 11:07:44 +0200

    creazione componenti

---
+++ spotify/src/components/Player.jsx
@@ -0,0 +1,35 @@
+const Player = () => {
+  return (
+    <div className="container-fluid fixed-bottom bg-container pt-1">
+      <div className="row h-100">
+        <div className="col-lg-10 offset-lg-2">
+          <div className="row h-100 flex-column justify-content-center align-items-center">
+            <div className="col-6 col-md-4 playerControls">
+              <div className="d-flex">
+                <a href="#">
+                  <img src="../shuffle.png" alt="shuffle" />
+                </a>
+                <a href="#">
+                  <img src="../prev.png" alt="prev" />
+                </a>
+                <a href="#">
+                  <img src="../play.png" alt="play" />
+                </a>
+                <a href="#">
+                  <img src="../next.png" alt="next" />
+                </a>
+                <a href="#">
+                  <img src="../repeat.png" alt="repeat" />
+                </a>
+              </div>
+              <div className="progress mt-3">
+                <div role="progressbar"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Player;
